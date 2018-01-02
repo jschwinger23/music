8630406feaaeef610e705c611bae9f9cfa488689
zc <greysavior@foxmail.com>
2018-01-03 01:04:51 +0800

    post_mortem

--- music/cli.py
+++ music/cli.py
@@ -8,12 +8,14 @@ from .libs.qq_music import QQMusic
 @click.command()
 @click.option('-f', '--music-file', type=click.Path(exists=True))
 @click.option('-d', '--music-dir', type=DirType())
-def musik(music_file, music_dir, qq_cookies):
-    qq_music = QQMusic(qq_cookies)
+@click.option('-c', '--qq-cid')
+@click.option('-u', '--qq-uid')
+def musik(music_file, music_dir, qq_cid, qq_uid):
+    qq_music = QQMusic(qq_cid, qq_uid)
 
     if music_file:
         music = Music.from_filename(music_file)
-        music.fetch_info_from_qq(qq_music)
+        music.update_from_qqmusic(qq_music)
 
     elif music_dir:
         for music_filename in music_dir.glob('.mp3'):
@@ -26,4 +28,10 @@ def musik(music_file, music_dir, qq_cookies):
 
 
 if __name__ == '__main__':
-    musik()
+    try:
+        musik()
+    except:
+        import sys, pdb, traceback
+        _, _, tb = sys.exc_info()
+        traceback.print_exc()
+        pdb.post_mortem(tb)
